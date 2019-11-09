1b279e6005c89c21c80ac010276186f9231fddd9
D-Bhatta <dbhatta1232@gmail.com>
2019-11-09 06:05:54 -0500

    Modified classifier.py to fleshout analyse_accuracy(self) and model_predictions(self)
    model_predictions(self):
    this function will now predict results by loading models stored on disk, and store the results in an array.
    analyse_accuracy(self):
    this function will now calculate and store the accuracy of results in an array.

--- classifier.py
+++ classifier.py
@@ -4,30 +4,64 @@ class Classifier(object):
     # define init function
     # add input variables into help string of class.
     def __init__(self,data):
-        self.data = []
-        self.data = data
         self.predictions = []
         self.prediction = ""
         self.accuracy = ""
         self.positive_probability = 0.00
         self.negative_probability = 0.00
+        from numpy import array
+        self.data = array([data])
+        # TODO: load model names and accuracy from file
+        # TODO: sort these models by accuracy in descending order
+        self.models = [('decision_tree_model.sav',74.0), ('deep_learning_model.sav',80.2), ('gradient_boost_model.sav',79.2), ('knn_model.sav',78.0), ('svc_model.sav',79.7), ('random_forest_model.sav',78.6), ('logistic_reg_model.sav',76.6)]
     def model_predictions(self):
         '''Imports models, stores model predictions'''
+        #joblib is used to serialize loading and storing models
+        from joblib import load
+        from joblib import dump
         # load a model
-        # predict a data
-        # store the predictions
-        pass
+        for i in range(len(self.models)):
+            filename = self.models[i][0]
+            # predict a data
+            loaded_model = load(filename)
+            model_prediction = loaded_model.predict(self.data) 
+            model_prediction = list(model_prediction)[0]
+            # store the predictions
+            self.predictions.append([model_prediction,self.models[i][1]])
     def analyse_accuracy(self):
         '''Analyse accuracy of model predictions'''
-        # calculate total accuracy of positive result
-        # calculate total accuracy of negative result
+        self.positive_probability = 0.00
+        self.negative_probability = 0.00
+        positive_probability = 0.00
+        negative_probability = 0.00
+        total_probability = 0.00
+        for i in range(len(self.predictions)):
+            # calculate total accuracy of positive result
+            if self.predictions[i][0] == 1:
+                positive_probability += self.predictions[i][1]
+            # calculate total accuracy of negative result
+            else:
+                negative_probability += self.predictions[i][1]
+            total_probability += self.predictions[i][1]
         # divide total accuracy of positive result by total accuracy and store in self.positive_probability as percentage
+        self.positive_probability = (positive_probability/total_probability)*100
         # divide total accuracy of negative result by total accuracy and store in self.negative_probability as percentage
-        pass
+        self.negative_probability = (negative_probability/total_probability)*100
     def predict_diabetes(self):
         '''Predicts whether there is diabetes or not'''
         # check whether positive probability or negative probability is higher
-        # predict whether there is diabetes or not
-        # construct prediction string
-        # construct accuracy string
-        pass
+        if self.positive_probability > self.negative_probability:
+            # predict whether there is diabetes or not
+            # construct prediction string
+            self.prediction = "Diabetes detectecion is positive"
+            # construct accuracy string
+            self.accuracy = "Accuracy of prediction is " + str(self.positive_probability)
+        elif self.positive_probability < self.negative_probability:
+            # construct prediction string
+            self.prediction = "Diabetes detectecion is negative"
+            # construct accuracy string
+            self.accuracy = "Accuracy of prediction is " + str(self.negative_probability)
+        else:
+            self.prediction = "Diabetes detectecion is inconclusive"
+            # construct accuracy string
+            self.accuracy = "Accuracy of prediction is " + str(self.positive_probability)
